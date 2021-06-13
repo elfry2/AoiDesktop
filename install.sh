1d69f92645eec96cdd480cbbfc38d6447aca7cfc
Elfry <47256917+elfry2@users.noreply.github.com>
2021-06-14 01:09:28 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo Aoi Desktop Installer
+echo 
+echo This script must be run on a TTY. Before executing further, please make sure that you have a stable internet connection and that the current account is a sudoer.
+echo 
+read -p "Press any key to continue..."
+echo
+
+# Install the required pacman packages.
+sudo pacman -Sy plank ulauncher --noconfirm --needed
+
+# Copy the required config files.
+cp -rv config/* ~/.config
+
+echo
+echo Script finished. Please reboot before logging in to ensure that the changes are applied properly.
